db5960c8f75b364de59bcffec265e88e7565e47b
gabriel-alex93 <gabo9302@gmail.com>
2015-11-26 22:32:19 -0300

    avance gestion de estudiantes

---
+++ Sistema_Chowa_Dojo/Sistema_Chowa_Dojo/Apoderado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Chowa_Dojo
+{
+    class Apoderado
+    {
+        String Nombre;
+        String Apellido;
+        String Telefono;
+        String Rut;
+        public Apoderado(String Nombre, String Apellido,String Telefono, String Rut) {
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Telefono = Telefono;
+            this.Rut = Rut;
+
+        }
+    }
+}
